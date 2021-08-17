eb1099dbad3fe831451786696d2187809f477610
Jonathan Kuo <jonathanpkuo@gmail.com>
2021-08-17 13:38:02 -0700

    character to integer and back functions

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,3 +1,15 @@
 def caesar_cipher(string, key)
 
 end
+
+def numberize(array)
+    array.map do |x|
+        x = x.ord
+    end
+end
+
+def characterize(array)
+    array.map do |x|
+        x = x.ord
+    end
+end
