fb994beb87d3cba13d0d05a86ec557c96fff4a85
Jonathan Kuo <jonathanpkuo@gmail.com>
2021-08-17 13:38:23 -0700

    cipher function

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -13,3 +13,24 @@ def characterize(array)
         x = x.ord
     end
 end
+
+def cipher_shift(array, offset)
+    # binding.pry
+    array.map! do |x|
+        if !(x.between?(97, 122) || x.between?(65,90))
+            x = x
+        elsif x.between?(97, 122)
+            if (x + offset).between?(97, 122)
+                x += offset
+            else
+                x += offset - 26
+            end
+        elsif x.between?(65, 90)
+            if (x + offset).between?(65, 90)
+                x += offset
+            else
+                x += offset - 26
+            end
+        end
+    end
+end
